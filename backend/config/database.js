0f79f0455749804cb0121328a1f757f5cd863907
Diegue83 <135059873+Diegue83@users.noreply.github.com>
2026-08-05 22:58:45 -0600

    Modificacion del url del broker

--- backend/config/database.js
+++ backend/config/database.js
@@ -3,9 +3,8 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const connectDB = async () => {
   try {
-    console.log(process.env.MONGO_URI_PROD);
 
-    await mongoose.connect(process.env.MONGO_URI_PROD,{});
+    await mongoose.connect(process.env.MONGODB_URI,{});
 
     console.log("✅ MongoDB Atlas conectado correctamente");
   } catch (error) {
