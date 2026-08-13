7858114c00e9ae8aec9e1435e539c51d9ad74a43
Mildred <mildred09b@gmail.com>
2026-08-12 18:18:41 -0600

    Eliminacion de modulos innecesarios

--- mobile/backend/src/main/java/mx/utng/backend/models/Parcela.js.kt
+++
@@ -1,2 +0,0 @@
-package mx.utng.backend.models
-
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -25,4 +25,3 @@ dependencyResolutionManagement {
 rootProject.name = "EcoViñedos"
 include(":mobile")
 include(":wear")
-include(":mobile:backend")
